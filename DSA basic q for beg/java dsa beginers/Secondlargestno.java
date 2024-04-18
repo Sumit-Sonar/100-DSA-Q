066d0f0ef01f683edab2f1f2956d589851db61c4
Sumit Sonar <90107230+Sumit-Sonar@users.noreply.github.com>
2024-04-19 02:03:04 +0530

    Add files via upload

---
+++ DSA basic q for beg/java dsa beginers/Secondlargestno.java
@@ -0,0 +1,10 @@
+public class Secondlargestno {
+    public static void main(String[] args) {
+
+        int[] list = {1,2,3};
+        for(int i : list){
+            if(list[i] != list[1]);
+            System.out.println("second largest no : " + list[1]);
+        }
+    }
+}
